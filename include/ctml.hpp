e0a12b2cbf16a0db1cbb57a6671ca8064368154d
Saksham Goyal <sakgoy2001@gmail.com>
2024-06-19 20:25:44 -0400

    operator<<
    overload `operator<<` to allow easier printing

--- include/ctml.hpp
+++ include/ctml.hpp
@@ -33,6 +33,13 @@
 
 namespace CTML
 {
+    /** Allow sending the string value to ostreams to allow `std::cout << node;`
+     *  instead of having to do this: `std::cout << node.ToString();`
+     */
+    ostream& operator<<(std::ostream& os, Node& node) {
+		os << node.ToString();
+		return os;
+	}
     /**
      * Searches the original string and replaces all occurances of the specified
      * string.
